bba9ab394960f22a2cb670adda1819c53cc504b2
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2024-09-29 17:53:18 -0300

    Cambio el formateo del readme un poco
    Signed-off-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>

--- pdf.sh
+++ pdf.sh
@@ -1,6 +1,6 @@
 cat clase_* > .todo.md
 # perl -Mutf8 -CSD -pe 's/(?[ \p{Emoji} - [\001 - \377] ])//g' todo.md > limpio.md
 xelatex caratula.tex 
-pandoc-bin --pdf-engine=xelatex --toc=true .todo.md -o contenido.pdf
+pandoc --pdf-engine=xelatex --toc=true .todo.md -o contenido.pdf
 pdfunite caratula.pdf contenido.pdf resumen.pdf 
 exit $?
